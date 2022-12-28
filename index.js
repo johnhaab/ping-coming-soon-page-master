13d17bb5edd38752d3bf90ffa758d2e4fc8df9e8
John Haab <johnlhaab@gmail.com>
2022-12-28 18:16:55 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const inputEmail = document.getElementById
+("input");
+const submitBtn = document.getElementById
+("btn");
+const form = document.getElementById
+("form");
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (inputEmail.value.match(regEx)) {
+        console.log("valid");
+        form.classList.remove("error");
+    }else{
+        console.log("error");
+        form.classList.add("error");
+    }
+})
